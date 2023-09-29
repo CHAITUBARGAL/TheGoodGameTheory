b0ebd9ad244bb83a58020632a4601649871941e1
Chetan Bargal <chetan.bargal11@gmail.com>
2023-09-29 16:59:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-// src/App.js
-import React, { useEffect, useState } from 'react';
-import BeerList from './components/BeerList';
+import LoadingIndicator from "./components/LoadingIndicator";
 
 function App() {
-  const [beers, setBeers] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  useEffect(() => {
-    fetch('https://api.punkapi.com/v2/beers')
-      .then((response) => response.json())
-      .then((data) => setBeers(data));
-  }, []);
-
   return (
     <div className="App">
-      <h1>Beer Catalog</h1>
-      <input
-        type="text"
-        placeholder="Search for beers..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <BeerList
-        beers={beers.filter((beer) =>
-          beer.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )}
-      />
+      <LoadingIndicator />
     </div>
   );
 }
